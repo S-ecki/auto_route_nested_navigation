a4678f3bda5150e17939be44457ac875e8d37589
S-ecki <75510543+S-ecki@users.noreply.github.com>
2022-10-02 15:43:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# auto_route_nested_navigation
+ Educational repository for nested navigation with the auto_route package
---
+++ android/app/src/main/kotlin/com/example/auto_route_nested_navigation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.auto_route_nested_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
